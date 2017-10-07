daa42426e3d19e5757d8c5a17d6d7ecb2a71eac8
Eenae <z-line@my.com>
2017-10-07 12:14:43 +0300

    deployment of STQPreICO

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,17 +2,15 @@
 
 const _owners = ['0xaF9Eca973ba1bf87923d45dCE471181F77DE301e', '0xc21afA07cE196500D45cD13B835645a2B05760e7', '0x7e71a0514D727A2828209CF74f05686f382e545D'];
 
-const STQToken = artifacts.require("./STQToken.sol");
-const STQPreSale = artifacts.require("./STQPreSale.sol");
+const STQTokenAddress = '0x5c3a228510D246b78a3765C20221Cbf3082b44a4';
+const STQPreICO = artifacts.require("./STQPreICO.sol");
 
-const preSaleWallet = '0x0Eed5de3487aEC55bA585212DaEDF35104c27bAF';
+const saleWallet = '0x0Eed5de3487aEC55bA585212DaEDF35104c27bAF';
 
 
 module.exports = function(deployer, network) {
-  deployer.deploy(STQToken, _owners).then(function() {
-    return deployer.deploy(STQPreSale, STQToken.address, preSaleWallet);
-  });
+  deployer.deploy(STQPreICO, STQTokenAddress, saleWallet);
 
   // owners have to manually perform
-  // STQToken.setController(address of STQPreSale);
+  // STQToken.setController(address of STQPreICO);
 };
